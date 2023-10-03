75eb6e0b4641a1f501d051bd3482449aa7cf4c92
tsboudot <tsboudot@gmail.com>
2023-10-03 17:29:44 +0200

    Récupération OK des images depuis le Db.json

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,6 +6,11 @@ const Contact = () => {
         <div className='Contact'>
             <Navigation />
             <div className='contactContent'>
+                <div className="ContactContent_Header">
+                    <h1>Thomas Boudot</h1>
+                    <h3>Développeur Front-End Junior</h3>
+                    <p>Téléchargez mon CV</p>
+                </div>
                 <ContactForm />
             </div>
         </div>
